7f8c4d9e2148385cdc4145a5b8f99a164536da36
Data Detective <datadetectiveagent007@gmail.com>
2016-12-23 12:14:23 -0800

    Updates Jave to use callback. Added descriptors to index.html CSS changes

--- Scripts/JavaScript.js
+++ Scripts/JavaScript.js
@@ -1,12 +1,11 @@
-﻿function genQuote()
-{
+﻿
+function genQuote(getAPI){
     //Generate random category to display
     var subj = ''; //the random quote category
     var min = 1;
     var max = 8;
-    var rand = rand = Math.floor(Math.random() * max) + min;
-    switch (rand)
-    {
+    var rand = Math.floor(Math.random() * max) + min;
+    switch (rand){
         case 1:
             subj = 'inspire';
             break;
@@ -33,35 +32,60 @@
             break;
     }
 
-    $.getJSON("http://quotes.rest/qod.json?category=" + subj, function (data)
-    {
-        var auth = data.contents.quotes[0].author;
-        var quote = data.contents.quotes[0].quote;
-        var title = data.contents.quotes[0].title;
-        var pic = data.contents.quotes[0].background;
-
-        document.getElementById('front').style.backgroundImage = 'url(' + pic + ')';
-
-
-        $(".quote").html(quote);
-        $(".quote1").html(auth);
-        console.log(auth);
+    $.getJSON("http://quotes.rest/qod.json?category=" + subj, function (data){
+        auth = data.contents.quotes[0].author;
+        quote = data.contents.quotes[0].quote;
+        title = data.contents.quotes[0].title;
+        pic = data.contents.quotes[0].background;
+        // Create an object to hold info from API call
+        var info = {
+            auth: auth,
+            quote: quote,
+            title: title,
+            pic: pic
+        };
+        // This will pass object out of getQuote function
+        buildContent(info);
+        
     });
 }
 
-$(document).ready(function ()
-{
-    //var title = '';
-    //var auth = '';
-    //var quote = '';
-    //var pic = '';
-    genQuote();
-    document.getElementById('title').innerHTML = '"' + auth + '"';
+function buildContent(info) {
+    if(info.title){
+        document.getElementById('front').style.backgroundImage = 'url(' + pic + ')';
+        document.getElementById('title').textContent = title;
+        $(".quote").html(quote);
+        $(".quote1").html(auth);
+        buildTwitter(info);
+    } else {
+        document.getElementById('title').textContent = 'Sorry Only So Much Inspiration Available.'
+    }
+}
 
-    //$("#tweet").on("click", document.getElementById('title').innerHTML = '"' + auth + '"');
-    //console.log(auth);
+//function buildTwitter(info) {
+//    $("#tweet").on("click", console.log(info.title));
+//}
+
+//credit to theysaidso.com
+function theysaidso(){
+    window.open("https://theysaidso.com");
+}
+
+// Initial load of page with quote
+$(document).ready(function (){
+    genQuote();
     $("#getQuote").on("click", genQuote);
+    $("#theysaidso").on("click", theysaidso);
+    $("#tweet").on("click", function (info) {
+        console.log(info.title);
+    });
 });
+
+
+//var auth = '';
+//var quote = '';
+//var title = '';
+//var pic = '';
 
 
 // **** A string to view API object values -- Call it inside getJSON function ****
@@ -71,7 +95,7 @@ $(document).ready(function ()
 //    return ret;
 //});
 
-
+// ***** Othe quote API's *****
 //http://quotes.rest/qod.json
 //frome the site https://quotesondesign.com/api-v4-0/ API link http://quotesondesign.com/wp-json/posts or http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=
 //http://api.androidhive.info/contacts/
